9839e5937cca26178bde3ee9ed634edced4277d5
jjaviergalvez <jjaviergalvez@hotmail.com>
2017-08-04 20:53:22 -0600

    feat: write my solution to optimum_policy.py

--- search/optimum_policy.py
+++ search/optimum_policy.py
@@ -1,12 +1,16 @@
 # ----------
 # User Instructions:
 # 
-# Create a function compute_value which returns
-# a grid of values. The value of a cell is the minimum
-# number of moves required to get from the cell to the goal. 
-#
-# If a cell is a wall or it is impossible to reach the goal from a cell,
-# assign that cell a value of 99.
+# Write a function optimum_policy that returns
+# a grid which shows the optimum policy for robot
+# motion. This means there should be an optimum
+# direction associated with each navigable cell from
+# which the goal can be reached.
+# 
+# Unnavigable cells as well as cells from which 
+# the goal cannot be reached should have a string 
+# containing a single space (' '), as shown in the 
+# previous video. The goal cell should have '*'.
 # ----------
 
 grid = [[0, 1, 0, 1, 0, 0],
@@ -14,6 +18,11 @@ grid = [[0, 1, 0, 1, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 0, 1, 0, 0],
         [0, 0, 0, 1, 0, 0]]
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0]]        
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1 # the cost associated with moving from a cell to an adjacent one
 
@@ -24,7 +33,7 @@ delta = [[-1, 0 ], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
-def compute_value(grid,goal,cost):
+def optimum_policy(grid,goal,cost):
     # ----------------------------------------
     # insert code below
     # ----------------------------------------
@@ -33,10 +42,12 @@ def compute_value(grid,goal,cost):
     # demonstrated in the previous video.
     value = [[99 for col in range(len(grid[0]))] for row in range(len(grid))]
     closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
     # initialization
     value[goal[0]][goal[1]] = 0
     closed[goal[0]][goal[1]] = 1
+    policy[goal[0]][goal[1]] = '*'
     
     lst = [goal]
     for e in lst:
@@ -51,9 +62,10 @@ def compute_value(grid,goal,cost):
                     value[x2][y2] = step
                     lst.append([x2,y2])
                     closed[x2][y2] = 1
+                    policy[x2][y2] = delta_name[(i+2)%4]
 
-    return value 
+    return policy
 
-result = compute_value(grid, goal, cost)
+result = optimum_policy(grid, goal, cost)
 for row in result:
     print(row)
