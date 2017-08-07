6df897faf20120d008114ebb7ff10e4245c1320b
jjaviergalvez <jjaviergalvez@hotmail.com>
2017-08-07 13:58:58 -0600

    feat: unsolved problem by myself

--- search/left-turn-policy.py
+++ search/left-turn-policy.py
@@ -59,4 +59,94 @@ cost = [2, 1, 20] # cost has 3 values, corresponding to making
 
 def optimum_policy2D(grid,init,goal,cost):
 
-    return policy2D
+  
+  closed = [[[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))]]
+
+  policy = [[999 for col in range(len(grid[0]))] for row in range(len(grid))]
+
+  value = [[[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[0 for col in range(len(grid[0]))] for row in range(len(grid))]]
+
+  policy = [[[999 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[999 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[999 for col in range(len(grid[0]))] for row in range(len(grid))],
+           [[999 for col in range(len(grid[0]))] for row in range(len(grid))]]           
+
+  policy2D = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+
+  #closed[init[2]][init[0]][init[1]] = 1
+  #value[init[2]][init[0]][init[1]] = 0
+
+  #closed[goal[0]][goal[1]] = 1
+  #value[goal[0]][goal[1]] = 0
+  
+  for i in range(4):
+    policy[i][goal[0]][goal[1]] = '*'
+    closed[i][goal[0]][goal[1]] = 1
+
+  lst = [goal]
+  
+  """
+  for i in range(4):
+    policy[i][init[0]][init[1]] = '*'
+    closed[i][init[0]][init[1]] = 1
+
+  lst = [init]
+  """
+
+  for item in lst:
+    x = item[0]
+    y = item[1]
+    for orientation in range(4):
+      for i in range(3):
+        o2 = (orientation + action[i]) % 4
+        #print("o2", o2)
+        x2 = x + forward[o2][0]
+        y2 = y + forward[o2][1]
+        #print("[x,y]", [x,y])
+        #print("[x2,y2]", [x2,y2])
+        #input("Press Enter to continue...")
+        if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+          if closed[o2][x2][y2] == 0 and grid[x2][y2] == 0:
+            """
+            v2 = value[o2][x2][y2] + cost[i]
+            value[orientation][x][y] = v2
+            policy[orientation][x][y] = action_name[i]
+            closed[o2][x2][y2] = 1
+            """
+            v2 = value[orientation][x][y] + cost[i]
+            value[orientation][x2][y2] = v2
+            policy[orientation][x2][y2] = action_name[i]
+            closed[orientation][x2][y2] = 1
+
+            lst.append([x2,y2])
+
+    for row in policy[0]:
+      print(row)
+    print(" ")
+    for row in value[0]:
+      print(row)
+    print(" ")
+
+  """
+  for n in range(4):
+    for row in policy[n]:
+      print(row)
+    print(" ")
+
+  for n in range(4):
+    for row in value[n]:
+      print(row)
+    print(" ")
+  """
+
+  return policy
+
+result = optimum_policy2D(grid,init,goal,cost)
+#for row in result:
+#  print(row)
