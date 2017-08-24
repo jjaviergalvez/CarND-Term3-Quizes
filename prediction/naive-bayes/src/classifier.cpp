6b5891882af0a01d3e31400bb158fc2ffb2fb015
jjaviergalvez <jjaviergalvez@hotmail.com>
2017-08-23 23:40:14 -0600

    feat: separate data by class as first step

--- prediction/naive-bayes/src/classifier.cpp
+++ prediction/naive-bayes/src/classifier.cpp
@@ -33,6 +33,36 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 		labels - array of N labels
 		  - Each label is one of "left", "keep", or "right".
 	*/
+
+	int n = data[0].size(); // number of features
+	int m = data.size(); // number of instances
+	int k = possible_labels.size(); // number of classes
+
+	double mean[3][n]; // to store the means 
+	double var[3][n]; // to store the variances
+
+	// Separate data by class
+	// separated[0] will contain all the instances for left turn
+	// separated[1] will contain all the instances for keep going
+	// separated[2] will contain all the instances for right turn
+	vector< vector< vector<double >>> separated (k);
+
+	for(int i = 0; i < m; i++)
+		for(int j = 0; j < k; j++)
+			if(labels[i] == possible_labels[j])
+    			separated[j].push_back(data[i]);
+
+    
+
+    cout << "data: " << separated[0][0][3] << endl;
+    cout << "data: " << separated[0][1][3] << endl;
+
+    cout << "data: " << separated[1][0][3] << endl;
+    cout << "data: " << separated[1][1][3] << endl;
+
+    cout << "data: " << separated[2][0][3] << endl;
+    cout << "data: " << separated[2][1][3] << endl;
+    	
 }
 
 string GNB::predict(vector<double> sample)
